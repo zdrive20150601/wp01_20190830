5c9004db7050cbeaf98dc5ad5e9ef5ff1baa8b94
zdis53 <zdrive20150601@gmail.com>
2019-08-30 11:29:37 +0900

    タイトル修正

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     	<?php if (have_posts()): ?>
     	<?php while(have_posts()): the_post(); ?>
           <article class="post">
-            <h2>あいうえお
+            <h2>かきくけこ
               <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             </h2>
             <p class="postmeta">
